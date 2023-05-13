defcae2a2248c3e5fec00fc1f16a50665278c029
MasterLox <102184514+MasterLox@users.noreply.github.com>
2023-05-13 22:38:36 +0200

    Add files via upload

---
+++ LoraRcV1/LoraRxV1.ino
@@ -0,0 +1,98 @@
+#include <ESP32Servo.h>
+#include <SPI.h>
+#include <LoRa.h>
+#define ss 18
+#define rst 14
+#define dio0 26
+
+byte escP = 22; 
+byte servo1P = 0;
+byte servo2P = 0;
+byte servo3P = 0;
+byte switch1P = 0;
+byte switch2P = 0;
+
+Servo esc;
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+Servo switch1;
+Servo switch2;
+
+int ppmChannel1, ppmChannel2, ppmChannel3, ppmChannel4, ppmChannel5, ppmChannel6;
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin(5, 19, 27, 18);
+  LoRa.setPins(ss, rst, dio0);
+  LoRa.begin(433E6);
+
+  esc.attach(escP);
+  servo1.attach(servo1P);
+  servo2.attach(servo2P);
+  servo3.attach(servo3P);
+  switch1.attach(switch1P);
+  switch2.attach(switch2P);
+
+  esc.writeMicroseconds(1000); // Arm ESC
+  delay(500); 
+}
+
+void setChannels(){
+ if (LoRa.parsePacket()) {
+    String packet = "";
+    while (LoRa.available()) {
+      packet += (char)LoRa.read();
+    }
+  
+
+    if (packet.startsWith("S") && packet.endsWith("E")) {
+      packet = packet.substring(1, packet.length() - 1);
+      int commaPos = packet.indexOf(",");
+      if (commaPos != -1) {
+        ppmChannel1 = packet.substring(0, commaPos).toInt();
+        packet = packet.substring(commaPos + 1);
+
+        commaPos = packet.indexOf(",");
+        if (commaPos != -1) {
+          ppmChannel2 = packet.substring(0, commaPos).toInt();
+          packet = packet.substring(commaPos + 1);
+
+          commaPos = packet.indexOf(",");
+          if (commaPos != -1) {
+            ppmChannel3 = packet.substring(0, commaPos).toInt();
+            packet = packet.substring(commaPos + 1);
+
+            commaPos = packet.indexOf(",");
+            if (commaPos != -1) {
+              ppmChannel4 = packet.substring(0, commaPos).toInt();
+              packet = packet.substring(commaPos + 1);
+
+              commaPos = packet.indexOf(",");
+              if (commaPos != -1) {
+                ppmChannel5 = packet.substring(0, commaPos).toInt();
+                ppmChannel6 = packet.substring(commaPos + 1).toInt();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+void driveComponents(){
+  esc.writeMicroseconds(ppmChannel1);
+  servo1.writeMicroseconds(ppmChannel2);
+  servo2.writeMicroseconds(ppmChannel3);
+  servo3.writeMicroseconds(ppmChannel4);
+  switch1.writeMicroseconds(ppmChannel5);
+  switch2.writeMicroseconds(ppmChannel6);
+}
+
+void loop() {  
+    setChannels();
+    driveComponents();
+    Serial.println(ppmChannel1);
+    delay(10);
+}
---
+++ LoraRcV1/LoraTxV1.ino
@@ -0,0 +1,73 @@
+#include <SPI.h>
+#include <LoRa.h>
+#define ss 18
+#define rst 14
+#define dio0 26
+
+// PPM signal input pin
+int ppmPin = 22;
+
+// PPM signal array
+volatile uint16_t ppmValues[8];
+
+// PPM signal counter
+volatile uint8_t ppmCount = 0;
+
+// PPM signal last received time
+volatile unsigned long ppmLastTime = 0;
+
+// PPM channel variables
+uint16_t ppmChannel1, ppmChannel2, ppmChannel3, ppmChannel4, ppmChannel5, ppmChannel6;
+
+void setup() {
+  // Set PPM signal pin as input
+  pinMode(ppmPin, INPUT);
+  SPI.begin(5, 19, 27, 18);
+  LoRa.setPins(ss, rst, dio0);
+  // Attach PPM signal interrupt
+  attachInterrupt(digitalPinToInterrupt(ppmPin), ppmInterrupt, CHANGE);
+
+  // Initialize serial communication
+  Serial.begin(9600);
+  LoRa.begin(433E6);
+}
+
+void loop() {
+ 
+  sendData();
+  delay(10);
+}
+
+void sendData() {
+  LoRa.beginPacket();
+ LoRa.print('S' + String(ppmChannel1) + ',' + String(ppmChannel2) + ',' + String(ppmChannel3) + ',' + String(ppmChannel4) + ',' + String(ppmChannel5) + ',' + String(ppmChannel6) + 'E');
+  LoRa.endPacket();
+}
+
+void ppmInterrupt() {
+  unsigned long ppmTime = micros();
+
+  // Calculate time since last PPM signal pulse
+  unsigned long ppmDelta = ppmTime - ppmLastTime;
+
+  if (ppmDelta > 2000) {
+    // End of PPM signal frame
+    ppmCount = 0;
+  } else if (ppmDelta > 500) {
+    // PPM signal pulse
+    ppmValues[ppmCount++] = ppmDelta;
+  }
+
+  if (ppmCount == 8) {
+    // All PPM channels have been received
+    ppmChannel1 = ppmValues[0] + 300;
+    ppmChannel2 = ppmValues[1] + 300;
+    ppmChannel3 = ppmValues[2] + 300;
+    ppmChannel4 = ppmValues[3] + 300;
+    ppmChannel5 = ppmValues[4] + 300;
+    ppmChannel6 = ppmValues[5] + 300;
+    ppmCount = 0;
+  }
+
+  ppmLastTime = ppmTime;
+}
